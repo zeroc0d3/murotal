c75fd2d6085aae855eb9f6cc06ddb9eccad96a8d
Dwi Fahni Denni (@zeroc0d3) <zeroc0d3.0912@gmail.com>
2021-11-27 22:24:36 +0700

    Added 8 Imam Murotal

---
+++ 03-abdurrahman-assudais.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PATH_MUROTAL="/home/zeroc0d3/Documents/murotal/Abdurrahman_AsSudais"
+DOWNLOAD_PATH="https://ia801904.us.archive.org/14/items/AbdurrahmanSudaisPerJuz"
+
+mkdir -p $PATH_MUROTAL
+cd $PATH_MUROTAL
+
+counter=1
+
+while [ $counter -le 30 ]
+do
+    if [ $counter -lt 10 ]
+    then
+      echo =============================
+      echo " Download Juz: 0$counter "
+      echo =============================
+      echo " >> $DOWNLOAD_PATH/0$counter.mp3"
+      wget $DOWNLOAD_PATH/0$counter.mp3
+    else
+      echo =============================
+      echo " Download Juz: $counter "
+      echo =============================
+      echo " >> $DOWNLOAD_PATH/$counter.mp3"
+      wget $DOWNLOAD_PATH/$counter.mp3
+    fi
+    ((counter++))
+    echo ""
+done
+
+echo "-- ALL DONE --"
---
+++ 07-nasser-alqatami.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PATH_MUROTAL="/home/zeroc0d3/Documents/murotal/Nasser_AlQatami"
+DOWNLOAD_PATH="https://ia800400.us.archive.org/23/items/Nasseralqatamieveryjuz"
+
+mkdir -p $PATH_MUROTAL
+cd $PATH_MUROTAL
+
+counter=1
+
+while [ $counter -le 30 ]
+do
+    if [ $counter -lt 10 ]
+    then
+      echo =============================
+      echo " Download Juz: 0$counter "
+      echo =============================
+      echo " >> $DOWNLOAD_PATH/0$counter.mp3"
+      wget $DOWNLOAD_PATH/0$counter.mp3
+    else
+      echo =============================
+      echo " Download Juz: $counter "
+      echo =============================
+      echo " >> $DOWNLOAD_PATH/$counter.mp3"
+      wget $DOWNLOAD_PATH/$counter.mp3
+    fi
+    ((counter++))
+    echo ""
+done
+
+echo "-- ALL DONE --"
